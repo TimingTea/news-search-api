2fa6bd4517d1fd88ba912c70a51b5909427ebb03
Timur <bolganbayev@yandex.ru>
2020-06-19 21:01:56 +0300

    fix cors

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const router = require('./routes/index');
 const limiter = require('./config/limiter');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors({ origin: ['http://localhost:8080', 'https://api.news-search.gq', 'https://www.api.news-search.gq'], credentials: true }));
 
 mongoose.connect('mongodb://localhost:27017/news-search', {
   useNewUrlParser: true,
